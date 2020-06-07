0374a587bbf798c95af01ab2c7ab0801a479fe79
pocket0207 <pocpoc0202@naver.com>
2020-06-07 22:33:04 +0900

    FX 지움

--- TestJavaFx/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module cse.jungdaekyeom.testjavafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens cse.jungdaekyeom.testjavafx to javafx.fxml;
-    exports cse.jungdaekyeom.testjavafx;
-}
